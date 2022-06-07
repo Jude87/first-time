bafd40e7065d2ced10f60dd583a26828f63fa82c
Jude <arima0210@gmail.com>
2022-06-07 23:05:56 +0900

    frist commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const daysOfweek = ["mon","tue","wed","thu","fri","sat","sun"]
+
+console.log(daysOfweek)
+
+daysOfweek.push("jude")
+
+console.log(daysOfweek)
+
+const ps5 = ["Dark Soul","Cyber Funk"]
+console.log(ps5)
+ps5.push("Final Fantasy9")
+console.log(ps5)
+console.log(ps5[2])
+
